a0e6b439b0139285834e2b7d4703add86f7e66cc
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2014-02-08 19:03:53 +0400

    Effect logic, reset button

--- src/chms/ru/magisso/MainActivity.java
+++ src/chms/ru/magisso/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +16,8 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+	private static final String CURRENT_IMAGE_URI_KEY = "currentImageUri";
 
 	private static final int MEDIA_LIB_REQUEST_CODE = 0;
 
@@ -67,7 +70,7 @@ public class MainActivity extends Activity {
 		Log.i(TAG, "decode");
 		try {
 			InputStream s = getContentResolver().openInputStream(uri);
-			// Check image size to not load too large image
+			// Check image size
 			BitmapFactory.Options opt = new BitmapFactory.Options();
 			opt.inJustDecodeBounds = true;
 			Bitmap b = BitmapFactory.decodeStream(s, null, opt);
@@ -95,27 +98,82 @@ public class MainActivity extends Activity {
 
 			currentBitmap = Bitmap.createBitmap(b.getWidth(), b.getHeight(),
 					Bitmap.Config.ARGB_8888);
-			
+
 			Canvas canvas = new Canvas(currentBitmap);
 			canvas.drawBitmap(b, 0, 0, null);
 
 			b.recycle();
 			ivImage.setImageBitmap(currentBitmap);
-
+			
+			setCurrentImageUri(uri);
 			return true;
 		} catch (Exception e) {
 			Log.e(TAG, "error while decoding image", e);
 			return false;
 		}
 	}
+	
+	/**
+	 * Set current image uri
+	 */
+	private void setCurrentImageUri(Uri uri){
+		getPreferences(MODE_PRIVATE).edit().putString(CURRENT_IMAGE_URI_KEY, uri.toString()).commit();
+	}
+	
+	/**
+	 * Get current image uri
+	 * @return Current image uri, if there is no image then return empty string ("") 
+	 */
+	private String getCurrentImageUri(){
+		return getPreferences(MODE_PRIVATE).getString(CURRENT_IMAGE_URI_KEY, "");
+	}
 
 	/**
-	 * Make random visual effect
+	 * How much line we draw
+	 */
+	private final int LINES_COUNT = 200;
+
+	/**
+	 * Available colors for lines
+	 */
+	private final int COLORS[] = { 0xffed1c24, 0xfff26522, 0xfffbaf5d,
+			0xfffff200, 0xff8dc63f, 0xff39b54a, 0xff00a651, 0xff00a99d,
+			0xff00aeef, 0xff0072bc, 0xff2e3192, 0xff662d91, 0xff92278f,
+			0xffec008c, 0xffed145b };
+
+	/**
+	 * Make random visual effect with lines
 	 * 
 	 * @param v
 	 */
 	public void makeMagic(View v) {
 		Log.i(TAG, "make magic");
+		Canvas canvas = new Canvas(currentBitmap);
+		Paint p = new Paint();
+
+		int maxWidth = ivImage.getWidth();
+		int maxHeight = ivImage.getHeight();
+
+		int linesLeft = LINES_COUNT;
+
+		while (linesLeft > 0) {
+			// randomize color
+			int color = COLORS[(int) (Math.random() * COLORS.length)];
+			p.setColor(color);
+			// randomize alpha
+			p.setAlpha((int) (Math.random() * 155 + 150));
+
+			// randomize coordinates
+			int x1, y1, x2, y2;
+			x1 = (int) (Math.random() * maxWidth);
+			x2 = (int) (Math.random() * maxWidth);
+			y1 = (int) (Math.random() * maxHeight);
+			y2 = (int) (Math.random() * maxHeight);
+			canvas.drawLine(x1, x2, y1, y2, p);
+			linesLeft--;
+		}
+
+		ivImage.setImageBitmap(currentBitmap);
 	}
 
 	/**
@@ -134,6 +192,16 @@ public class MainActivity extends Activity {
 	 */
 	public void share(View v) {
 		Log.i(TAG, "share");
+	}
+
+	/**
+	 * Reset image in image view
+	 * @param v
+	 */
+	public void resetImage(View v) {
+		Log.i(TAG, "reset");
+		String uri = getCurrentImageUri();
+		if(uri!=""){ decodeImageAndPutInImageView(Uri.parse(uri)); }
 	}
 
 	/**
