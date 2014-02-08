6653db14a97cfb59f72ae5b1bba2e2726698ac84
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2014-02-08 17:34:12 +0400

    UI event listeners

--- src/chms/ru/magisso/MainActivity.java
+++ src/chms/ru/magisso/MainActivity.java
@@ -2,9 +2,13 @@ package chms.ru.magisso;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
+
+	private static final String TAG = "MainActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,43 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	/**
+	 * Load image
+	 * @param v
+	 */
+	public void loadImage(View v){
+		Log.i(TAG, "load image");
+	}
+	
+	/**
+	 * Make random visual effect
+	 * @param v
+	 */
+	public void makeMagic(View v){
+		Log.i(TAG, "make magic");
+	}
+	
+	/**
+	 * Click on save button
+	 * @param v
+	 */
+	public void onBtnSaveClick(View v){
+		Log.i(TAG, "btn save click");
+	}
+	
+	/**
+	 * Share
+	 * @param v
+	 */
+	public void share(View v){
+		Log.i(TAG, "share");
+	}
+	
+	/**
+	 * Saves current image
+	 */
+	public void save(){
+		Log.i(TAG, "save");
+	}
 }
